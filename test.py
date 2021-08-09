19f7b719e7a9d1cbc3da6e51bd7e6a15b4d837b6
VicW Dump <75227935+VicW-dump@users.noreply.github.com>
2021-08-09 11:18:45 +0200

    Create test.py

---
+++ test.py
@@ -0,0 +1,7 @@
+from pysheeter.PySheeter import PySheeter
+
+# Load sprites from './files/' (all sprites are 64x64)
+spritesheet = PySheeter.Sheet("example/files")
+
+# Create a vertical spritesheet with the dimensions 16x16 (scaled)
+spritesheet.put("example_v1616.png",(16,16))
